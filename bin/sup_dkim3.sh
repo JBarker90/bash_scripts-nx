73fa253a8de699e36df1c7b96d29cb38726af2bf
Jonathan Barker <jonathan.barker09@gmail.com>
2023-02-24 21:38:18 -0500

    fixed usage/help function to include correct echo statements

--- bin/sup_dkim3.sh
+++ bin/sup_dkim3.sh
@@ -8,19 +8,17 @@ help(){
     echo "This bash script can be used to check if a DKIM key exists for a domain and generate one if needed."
     echo "It can also find the DKIM public key file and output the DKIM value in a DNS record format."
     echo 
-    echo "Syntax: sup_dkim2.sh -d [<domain>] [-h|c|f]"
+    echo "Syntax: sup_dkim3.sh [-h|c|f] [<domain>]"
     echo "options:"
     echo "-h    Print this Help message."
-    echo "-d    Specify Domain."
     echo "-c    Create a DKIM Key for a specific domain and formats it in DNS record values."
     echo "-f    Can be used to Force a new DKIM key to be generated."
 }
 
 function usage(){
-    echo "Syntax: sup_dkim2.sh -d [<domain>] [-h|c|f]"
+    echo "Syntax: sup_dkim3.sh [-h|c|f] [<domain>]"
     echo "options:"
     echo "-h    Print this Help message."
-    echo "-d    Specify Domain."
     echo "-c    Create a DKIM Key for a specific domain and formats it in DNS record values."
     echo "-f    Can be used to Force a new DKIM key to be generated."
 }
