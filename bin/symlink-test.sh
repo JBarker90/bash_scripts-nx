05e25ae16880040a36617385f57811b6db1ad370
JBarker90 <jonathan.barker09@gmail.com>
2023-09-16 23:01:39 -0400

    added to script and moved rm temp variable

--- bin/symlink-test.sh
+++ bin/symlink-test.sh
@@ -2,13 +2,15 @@
 
 DOMAIN=$1
 if [ -L "/Users/jbarker/$DOMAIN" ]; then
-  tmp=$(mktemp)
-  link_target=$(readlink -f /Users/jbarker/"$DOMAIN")
-  echo "Symbolic link exists and points to $link_target"
-  echo "Temporary variable created: $tmp"
-  # Do something with the temporary variable and link_target here
+    tmp=$(mktemp)
+    link_target=$(readlink -f /Users/jbarker/"$DOMAIN")
+    echo "Symbolic link exists and points to $link_target"
+    echo "Temporary variable created: $tmp"
+    # Do something with the temporary variable and link_target here
+    rm "$tmp"
+elif [[ -e "/Users/jbarker/$DOMAIN" ]]; then
+    file_target=$(readlinke -f /Users/jbarker/"$DOMAIN")
+    echo "This file arelady exists $file_target"
 else
   echo "Symbolic link does not exist"
 fi
-
-rm "$tmp"
